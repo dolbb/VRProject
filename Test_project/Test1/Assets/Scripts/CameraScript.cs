8c847df18b69b53adbab986b5f37b76f79eb6d65
LevNetanel <38345761+LevNetanel@users.noreply.github.com>
2018-07-28 13:55:14 +0300

    update

---
+++ Test_project/Test1/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	public Vector3 worldPoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		updateWorldPoint ();
+	}
+
+	void updateWorldPoint()
+	{
+		Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit))
+		{
+			worldPoint = hit.point;
+			return;
+		}
+		worldPoint = Vector3.zero; // Never reaching here
+	}
+}
---
+++ Test_project/Test1/Assets/Scripts/EditScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditScript : MonoBehaviour {
+
+	public GameObject mainCamera;
+	CameraScript camScript;
+
+	bool creating;
+	public GameObject start;
+	public GameObject end;
+
+	public GameObject wallPrefab;
+	GameObject wall;
+
+	// Use this for initialization
+	void Start()
+	{
+		camScript = mainCamera.GetComponent<CameraScript>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		getInput();
+	}
+
+	void getInput()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			setStart();
+		}
+		else if (Input.GetMouseButtonUp(0))
+		{
+			setEnd();
+		}
+		else
+		{
+			if (creating)
+				adjust();
+		}
+	}
+
+	void setStart()
+	{
+		creating = true;
+		start.transform.position = camScript.worldPoint;
+		wall = (GameObject)Instantiate(wallPrefab, start.transform.position, Quaternion.identity);
+	}
+
+	void setEnd()
+	{
+		creating = false;
+		end.transform.position = camScript.worldPoint;
+	}
+
+	void adjust()
+	{
+		end.transform.position = camScript.worldPoint;
+		adjustWall();
+	}
+
+	void adjustWall()
+	{
+		start.transform.LookAt(end.transform.position);
+		end.transform.LookAt(start.transform.position);
+		float distance = Vector3.Distance(start.transform.position, end.transform.position);
+		wall.transform.position = start.transform.position + distance / 2 * start.transform.forward;
+		wall.transform.rotation = start.transform.rotation;
+		wall.transform.localScale = new Vector3(wall.transform.localScale.x, wall.transform.localScale.y, distance);
+	}
+
+}
